b2f14be029d80ff7b9658551911ae7aeea24574a
Federico Borello <156438142+fborello-lambda@users.noreply.github.com>
2024-08-15 18:53:59 -0300

    fix: delete git merge watermarks (#17)

--- utils/commands/index.ts
+++ utils/commands/index.ts
@@ -2,11 +2,8 @@ import { getBalance } from "./cmdGetBalance";
 import { sendBalance } from "./cmdSendBalance";
 import { depositBalance } from "./cmdDeposit";
 import { fixNonce } from "./cmdFixNonce";
-<<<<<<< HEAD
 import { loadTest } from "./loadTester/cmdLoadTest";
-=======
 import { getRandomWallet } from "./cmdGetRandomWallet";
->>>>>>> main
 
 export const cmd = {
     balance: {
