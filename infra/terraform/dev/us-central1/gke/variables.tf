1729a5d420ee7308f30fddc9a290fcb6a1f555ec
Klaus Lungwitz <klaus.lungwitz@lambdaclass.com>
2024-09-13 15:17:51 -0300

    Change location of GPU nodes

--- infra/terraform/dev/us-central1/gke/variables.tf
+++ infra/terraform/dev/us-central1/gke/variables.tf
@@ -32,7 +32,7 @@ variable "cpu_nodes_per_zone" {
 
 variable "gpu_nodes_locations" {
   type    = string
-  default = "us-central1-c"
+  default = "us-central1-b"
 }
 
 variable "gpu_nodes_per_zone" {
